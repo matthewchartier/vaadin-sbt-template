2a670f449bb817de0b3ff793d9548699e5464354
Matthew Chartier <mattc@eyewyre.com>
2016-04-01 16:55:38 -0400

    added graph

--- build.sbt
+++ build.sbt
@@ -11,13 +11,14 @@ resolvers ++= Seq(
 )
 
 libraryDependencies ++= Seq(
-  "com.vaadin" % "vaadin-server" % "7.6.0",
-  "com.vaadin" % "vaadin-client" % "7.6.0",
-  "com.vaadin" % "vaadin-client-compiler" % "7.6.0",
-  "com.vaadin" % "vaadin-client-compiled" % "7.6.0",
-  "com.vaadin" % "vaadin-themes" % "7.6.0",
-//  "com.vaadin.addon" % "vaadin-charts" % "2.1.3",
-//  "com.vaadin.addon" % "vaadin-spreadsheet" % "1.1.1",
+  "com.vaadin" % "vaadin-server" % "7.6.4",
+  "com.vaadin" % "vaadin-client" % "7.6.4",
+  "com.vaadin" % "vaadin-client-compiler" % "7.6.4",
+  "com.vaadin" % "vaadin-client-compiled" % "7.6.4",
+  "com.vaadin" % "vaadin-themes" % "7.6.4",
+  "com.vaadin.addon" % "vaadin-charts" % "3.0.0",
+  "com.vaadin.addon" % "vaadin-spreadsheet" % "1.1.6",
+//  "com.vaadin.addon" % "vaadin-touchkit-cval" % "4.1.0",
   "org.vaadin.addons" % "scaladin" % "3.1.0",
   "javax.servlet" % "javax.servlet-api" % "3.0.1" % "provided",
   "com.typesafe.play" % "play-ws_2.11" % "2.4.6",
--- src/main/scala/com/eyewyre/AppUI.scala
+++ src/main/scala/com/eyewyre/AppUI.scala
@@ -3,11 +3,14 @@ package com.eyewyre
 
 import _root_.java.io.File
 
-import com.vaadin.annotations.{Theme,PreserveOnRefresh}
-import com.vaadin.server.{VaadinService, VaadinRequest, Sizeable}
+import com.vaadin.annotations.{PreserveOnRefresh, Theme}
+import com.vaadin.server.{Sizeable, VaadinRequest, VaadinService}
 import com.vaadin.ui._
-import com.vaadin.ui.Button.{ClickListener, ClickEvent}
+import com.vaadin.ui.Button.{ClickEvent, ClickListener}
 import com.ning.http.client.AsyncHttpClientConfig
+import com.vaadin.addon.charts.Chart
+import com.vaadin.addon.charts.model.style.SolidColor
+import com.vaadin.addon.charts.model.{ChartType, DataSeries, DataSeriesItem}
 import play.api.libs.ws.{WS, WSResponse}
 import play.api.libs.ws.ning.NingWSClient
 
@@ -35,58 +38,59 @@ class AppUI extends UI {
       setStyleName("box")
     }
 
-    setContent(new HorizontalLayout() {
-      addComponent(box)
+    setContent(new VerticalLayout {
 
-      addComponent(new Button("Add") {
-        addClickListener(new ClickListener {
-          override def buttonClick(clickEvent: ClickEvent): Unit = {
-            val list = List(1,2,3,4,5,6,7,8)
-            val result: List[Int] = list.filter(_ > 2).sortWith((a, b) => b < a)
+      addComponent(new HorizontalLayout {
+        addComponent(box)
 
-            box.addStyleName("grow")
-          }
+        addComponent(new Button("Add") {
+          addClickListener(new ClickListener {
+            override def buttonClick(clickEvent: ClickEvent): Unit = {
+              Notification.show("Adding 'grow'")
+              box.addStyleName("grow")
+            }
+          })
+        })
+
+        addComponent(new Button("Remove") {
+          addClickListener(new ClickListener {
+            override def buttonClick(clickEvent: ClickEvent): Unit = {
+              Notification.show("Removing 'grow'")
+              box.removeStyleName("grow")
+            }
+          })
         })
       })
 
-      addComponent(new Button("Remove") {
-        addClickListener(new ClickListener {
-          override def buttonClick(clickEvent: ClickEvent): Unit = {
-            box.removeStyleName("grow")
-          }
+      //    client.url("http://localhost/api/authenticate/userpass")
+      //      .withFollowRedirects(follow = true)
+      //      .withHeaders("Content-Type" -> "application/json")
+      //      .post(s"""{ "username": "", "password": "" }""")
+      //      .map { response =>
+      //  Notification.show(response.body)
+      //    }
+
+
+      // val basePath = VaadinService.getCurrent.getBaseDirectory.getAbsolutePath
+      // val sampleFile = new File(basePath + "/Simple Invoice.xlsx")
+
+      //            addComponent(new Spreadsheet(sampleFile) {
+      //              setHeight(450, Sizeable.Unit.PIXELS)
+      //            })
+
+      addComponent(new Chart(ChartType.PIE) {
+        setWidth(500, Sizeable.Unit.PIXELS)
+        setHeight(500, Sizeable.Unit.PIXELS)
+
+        val conf = getConfiguration
+        conf.setTitle("Sample Pie")
+        conf.getChart.setBackgroundColor(new SolidColor(255, 255, 255, 0))
+        conf.setSeries(new DataSeries() {
+          1 to 5 foreach { idx => add(new DataSeriesItem(s"Slice $idx", idx * 100)) }
         })
+
+        drawChart(conf)
       })
-
-        //    client.url("http://localhost/api/authenticate/userpass")
-        //      .withFollowRedirects(follow = true)
-        //      .withHeaders("Content-Type" -> "application/json")
-        //      .post(s"""{ "username": "", "password": "" }""")
-        //      .map { response =>
-				//  Notification.show(response.body)
-        //    }
-
-
-            // val basePath = VaadinService.getCurrent.getBaseDirectory.getAbsolutePath
-            // val sampleFile = new File(basePath + "/Simple Invoice.xlsx")
-
-//            addComponent(new Spreadsheet(sampleFile) {
-//              setHeight(450, Sizeable.Unit.PIXELS)
-//            })
-
-            // addComponent(new Chart(ChartType.PIE) {
-            //   setWidth(300, Sizeable.Unit.PIXELS)
-            //   setHeight(300, Sizeable.Unit.PIXELS)
-            //
-            //   val conf = getConfiguration
-            //   conf.setTitle("Sample Pie")
-            //   conf.getChart.setBackgroundColor(new SolidColor(255, 255, 255, 0))
-            //   conf.setSeries(new DataSeries() {
-            //     1 to 5 foreach { idx => add(new DataSeriesItem(s"Slice $idx", idx * 100)) }
-            //   })
-            //
-            //   drawChart(conf)
-            // })
     })
   }
-
 }
